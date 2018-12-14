e7cd2f0d1135a6cf5681a0c9913c20ba42839a5b
Tristan Sloughter <t@crashfast.com>
2018-12-14 16:57:17 -0700

    use optional arguments to combine the macros

--- lib/opencensus/trace.ex
+++ lib/opencensus/trace.ex
@@ -1,22 +1,6 @@
 defmodule Opencensus.Trace do
-  @doc "Wraps the given block in a tracing child span with the given label/name"
-  defmacro with_child_span(label, do: block) do
-    quote do
-      parent_span_ctx = :ocp.current_span_ctx()
-      new_span_ctx = :oc_trace.start_span(unquote(label), parent_span_ctx, %{})
-      :ocp.with_span_ctx(new_span_ctx)
-
-      try do
-        unquote(block)
-      after
-        :oc_trace.finish_span(new_span_ctx)
-        :ocp.with_span_ctx(parent_span_ctx)
-      end
-    end
-  end
-
-  @doc "Wraps the given block in a tracing child span with the given label/name and additional attributes"
-  defmacro with_child_span(label, attributes, do: block) do
+  @doc "Wraps the given block in a tracing child span with the given label/name and optional attributes"
+  defmacro with_child_span(label, attributes \\ quote(do: %{}), do: block) do
     quote do
       parent_span_ctx = :ocp.current_span_ctx()
       new_span_ctx = :oc_trace.start_span(unquote(label), parent_span_ctx, unquote(attributes))
