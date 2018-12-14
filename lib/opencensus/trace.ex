94f878db2e010fd5cd8d6f7ab7c9c1681b07d744
Łukasz Niemier <lukasz@niemier.pl>
2018-12-15 00:55:18 +0100

    feat: provide additional context for span
    It adds additional context to the span, namely:
    
    - module
    - line
    - function where span started
    - file

--- lib/opencensus/trace.ex
+++ lib/opencensus/trace.ex
@@ -1,7 +1,19 @@
 defmodule Opencensus.Trace do
   @doc "Wraps the given block in a tracing child span with the given label/name and optional attributes"
   defmacro with_child_span(label, attributes \\ quote(do: %{}), do: block) do
+    line = __CALLER__.line
+    module = __CALLER__.module
+    file = __CALLER__.file
+    function = format_function(__CALLER__.function)
+
     quote do
+      attributes = Map.merge(unquote(attributes), %{
+        line: unquote(line),
+        module: unquote(module),
+        file: unquote(file),
+        function: unquote(function)
+      })
+
       parent_span_ctx = :ocp.current_span_ctx()
       new_span_ctx = :oc_trace.start_span(unquote(label), parent_span_ctx, unquote(attributes))
       :ocp.with_span_ctx(new_span_ctx)
@@ -14,4 +26,7 @@ defmodule Opencensus.Trace do
       end
     end
   end
+
+  defp format_function(nil), do: nil
+  defp format_function({name, arity}), do: "#{name}/#{arity}"
 end
