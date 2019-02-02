03c00ca6d18af6db1d8d64911ee5bb55265fdd61
Tristan Sloughter <t@crashfast.com>
2019-02-02 11:43:56 -0700

    move default attributes to separate macro

--- lib/opencensus/trace.ex
+++ lib/opencensus/trace.ex
@@ -1,6 +1,30 @@
 defmodule Opencensus.Trace do
   @doc "Wraps the given block in a tracing child span with the given label/name and optional attributes"
   defmacro with_child_span(label, attributes \\ quote(do: %{}), do: block) do
+    quote do
+      parent_span_ctx = :ocp.current_span_ctx()
+
+      new_span_ctx =
+        :oc_trace.start_span(unquote(label), parent_span_ctx, %{
+          :attributes => unquote(attributes)
+        })
+
+      :ocp.with_span_ctx(new_span_ctx)
+
+      try do
+        unquote(block)
+      after
+        :oc_trace.finish_span(new_span_ctx)
+        :ocp.with_span_ctx(parent_span_ctx)
+      end
+    end
+  end
+
+  @doc """
+  Constructs an attribute map from an optional map of attributes and attributes
+  for the current calling environment.
+  """
+  defmacro default_attributes(attributes \\ quote(do: %{})) do
     line = __CALLER__.line
     module = __CALLER__.module
     file = __CALLER__.file
@@ -14,17 +38,6 @@ defmodule Opencensus.Trace do
           file: unquote(file),
           function: unquote(function)
         })
-
-      parent_span_ctx = :ocp.current_span_ctx()
-      new_span_ctx = :oc_trace.start_span(unquote(label), parent_span_ctx, unquote(attributes))
-      :ocp.with_span_ctx(new_span_ctx)
-
-      try do
-        unquote(block)
-      after
-        :oc_trace.finish_span(new_span_ctx)
-        :ocp.with_span_ctx(parent_span_ctx)
-      end
     end
   end
 
--- test/opencensus_test.exs
+++ test/opencensus_test.exs
@@ -1,7 +1,38 @@
+defmodule Span do
+  require Record
+  Record.defrecord(:span, Record.extract(:span, from_lib: "opencensus/include/opencensus.hrl"))
+end
+
+defmodule PidAttributesReporter do
+  import Span
+
+  def init(pid) do
+    pid
+  end
+
+  def report(spans, pid) do
+    Enum.each(spans, fn span ->
+      send(pid, {:attributes, span(span, :attributes)})
+    end)
+  end
+end
+
 defmodule OpencensusTest do
   use ExUnit.Case
+  import Opencensus.Trace
 
-  test "do some span or something" do
-    assert true
+  test "verify attributes", _state do
+    :application.load(:opencensus)
+    :application.set_env(:opencensus, :send_interval_ms, 1)
+    :application.set_env(:opencensus, :reporter, {PidAttributesReporter, self()})
+
+    :application.ensure_all_started(:opencensus)
+    :application.ensure_all_started(:opencensus_elixir)
+
+    with_child_span "child_span", default_attributes(%{"attr-1" => "value-1"}) do
+      :do_something
+    end
+
+    assert_receive {:attributes, %{"attr-1" => "value-1", :module => OpencensusTest}}, 1_000
   end
 end
