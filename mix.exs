221c58ff92578e15fb7822ea36d6b8db503079b1
Tristan Sloughter <t@crashfast.com>
2019-07-05 09:45:38 -0600

    version 0.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OpencensusElixir.MixProject do
   def project do
     [
       app: :opencensus_elixir,
-      version: "0.3.0",
+      version: "0.4.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
